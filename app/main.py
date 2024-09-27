a1bc03c52b6b686f1d2f61bcb585b8a1fe8591ed
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2024-09-28 01:53:23 +0300

    implement py-car-wash-station

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -12,31 +17,34 @@ class CarWashStation:
             clean_power: int,
             average_rating: float,
             count_of_ratings: int
-    ):
+    ) -> None:
         self.total_price = 0
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list[Car]):
+    def serve_cars(self, cars: list[Car]) -> float:
         for car in cars:
             self.wash_single_car(car)
         return round(self.total_price, 1)
 
-    def calculate_washing_price(self, car: Car):
+    def calculate_washing_price(self, car: Car) -> float:
         return round(car.comfort_class
                      * (self.clean_power
                         - car.clean_mark)
                      * self.average_rating
                      / self.distance_from_city_center, 1)
 
-    def wash_single_car(self, car: Car):
+    def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             self.total_price += self.calculate_washing_price(car)
             car.clean_mark = self.clean_power
 
-    def rate_service(self, mark: int):
+    def rate_service(self, mark: int) -> None:
         total_rating = self.average_rating * self.count_of_ratings
         self.count_of_ratings += 1
-        self.average_rating = round((total_rating + mark) / self.count_of_ratings, 1)
+        self.average_rating = round(
+            (total_rating
+                + mark)
+            / self.count_of_ratings, 1)
